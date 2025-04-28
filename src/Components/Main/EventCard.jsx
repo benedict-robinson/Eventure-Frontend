a68c7c74492d9d642d4fc0ed19012cb603f1ddc7
Benedict Robinson <benedict.r1713@gmail.com>
2025-04-28 15:47:18 +0100

    Image Sizing

--- src/Components/Main/EventCard.jsx
+++ src/Components/Main/EventCard.jsx
@@ -8,12 +8,14 @@ export default function EventCard({ event }) {
   return (
     <div className="event-card">
       <Link to={`/event/${event.event_id}`}>
-        <img src={event.img.url} className="img" style={{width: "100%", height: "100px", objectFit: 'contain'}}/>
+        <div className="img-container">
+          <img src={event.img.url} className="img" style={{ width: "100%", height: "100%", objectFit: 'cover' }} />
+        </div>
         <h2>{event.name}</h2>
       </Link>
-        <p>{event.date_and_time.start_date}</p>
-        <p>{event.date_and_time.start_time}</p>
-        <p>Tags: {event.tags.map(t => <span id="tag">{t} </span>)}</p>
+      <p>{event.date_and_time.start_date}</p>
+      <p>{event.date_and_time.start_time}</p>
+      {event.tags.length > 0 ? <p>Tags: {event.tags.filter(t => t !== "Undefined").join(", ")}</p> : <></>}
       <button><HeartIconComp /></button>
       <button><CalendarIconComp /></button>
     </div>
