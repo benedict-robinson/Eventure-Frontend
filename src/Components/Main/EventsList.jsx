a4fcd4fde80052f5d4fe57d479fafadb38e94386
Benedict Robinson <benedict.r1713@gmail.com>
2025-05-04 11:09:03 +0100

    Responsive Design

--- src/Components/Main/EventsList.jsx
+++ src/Components/Main/EventsList.jsx
@@ -6,28 +6,28 @@ import Loader from "./Loader.jsx"
 import "./Loader.css"
 
 
-export default function EventsList({params, categoryName}) {
+export default function EventsList({ params, categoryName }) {
     const [events, setEvents] = useState([])
     const [isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
-        fetchEvents(params).then(({events: fetched}) => {
+        fetchEvents(params).then(({ events: fetched }) => {
             let newEvents = [...fetched]
             if (categoryName) {
                 const regex = new RegExp(categoryName, "i")
                 newEvents = newEvents.filter(e =>
                     e.tags.some(tag => regex.test(tag))
-                  );
+                );
             }
             setEvents(newEvents)
         })
-        .catch((err) => {
-            console.log({error: err})
-            setEvents([])
-        })
-        .finally(() => {
-            setIsLoading(false)
-        })
+            .catch((err) => {
+                console.log({ error: err })
+                setEvents([])
+            })
+            .finally(() => {
+                setIsLoading(false)
+            })
     }, [params, categoryName])
 
     if (isLoading) {
@@ -38,23 +38,23 @@ export default function EventsList({params, categoryName}) {
         )
     }
 
-if (events.length === 0) {
+    if (events.length === 0) {
+        return (
+            <div className="No-Events">
+                <h2>Hmm... Looks like there are no{categoryName ? ` ${categoryName}` : ""} events scheduled in {params.city}</h2>
+            </div>
+        )
+    }
+
     return (
-        <div className="No-Events">
-            <h2>Hmm... Looks like there are no{categoryName ? ` ${categoryName}` : ""} events scheduled in {params.city}</h2>
+        <div className="eventslist-container">
+            {events.map((e, i) => {
+                return (
+                    <div className="event-card-container">
+                        <EventCard key={i} event={e} />
+                    </div>
+                )
+            })}
         </div>
     )
-}
-
-  return (
-    <div className="eventslist-container">
-        {events.map((e, i) => {
-            return (
-                <div className="event-card-container">
-                    <EventCard key={i} event={e}/>
-                </div>
-            )
-        })}
-    </div>
-  )
 }
