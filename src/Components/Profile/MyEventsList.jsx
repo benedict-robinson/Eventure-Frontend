0f21fd9b627df732debc32563c43938481c6a2b1
Benedict Robinson <benedict.r1713@gmail.com>
2025-05-05 19:08:43 +0100

    Favicon

--- src/Components/Profile/MyEventsList.jsx
+++ src/Components/Profile/MyEventsList.jsx
@@ -2,10 +2,14 @@ import { useEffect, useState } from "react"
 import { fetchMyEvents } from "../../api.js"
 import EventCard from "../Main/EventCard.jsx"
 import Loader from "../Main/Loader.jsx"
+import { useNavigate } from "react-router-dom"
+import "./CSS/Lists.css"
 
 export default function MyEventsList({user}) {
   const [userMyEvents, setUserMyEvents] = useState([])
   const [isLoading, setIsLoading] = useState(true)
+  const navigate = useNavigate()
+
   useEffect(() => {
     if (!user || !user.user_id) return;
     fetchMyEvents(user.user_id)
@@ -20,11 +24,26 @@ export default function MyEventsList({user}) {
       })
   }, [user]);
 
+  function goToCreate() {
+    navigate("/account/create")
+  }
+
   if (isLoading) {
     return (
       <div className="loader-container">
         <Loader />
       </div>
+    )
+  }
+
+  if (userMyEvents.length < 1) {
+    return (
+      <div className="my-events-container">
+      <h2>My Events</h2>
+      <div className="lists-container">
+      <p onClick={goToCreate} id="create-new-tag">You haven't organised any events yet. Click here to create one!</p>
+      </div>
+    </div>
     )
   }
 
